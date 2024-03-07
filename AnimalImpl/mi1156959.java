91884279c25772a7b6455ed7e131643ea7d9a609
AzzieDev <18339356+AzzieDev@users.noreply.github.com>
2024-03-07 15:15:18 -0500

    Revert "Merge pull request #7 from CabbagePicasso/main"
    This reverts commit 44d1371e94314f751f6d8ddfc6f398b0388a8b8a, reversing
    changes made to 00202732d598e0c96dde3173d06265429f351566.

--- AnimalImpl/mi1156959.java
+++ AnimalImpl/mi1156959.java
@@ -3,27 +3,24 @@ package SeeAndSay.AnimalImpl;
 import SeeAndSay.Animal;
 import SeeAndSay.Talker;
 
-public class sa1153877 extends Animal implements Talker {
+public class mi1156959 extends Animal implements Talker {
 
     @Override
     public String getAuthor() {
-        return "Abby Salisbury";
+        return "Ethan Milligan";
     }
 
     @Override
     public String getAnimalName() {
-        return "Shark";
+        return "Dodo";
     }
 
     @Override
-    //BBC Science Focus (sharks don't make sound don't trust Jaws 4)
     public String getSound() {
-        return "I can smell blood from around a quarter mile away.";
+        return "Coo-coo";
     }
-
     @Override
     public String getDescription() {
-        return "Eager Shark";
+        return "Extinct Bird";
     }
-
 }
